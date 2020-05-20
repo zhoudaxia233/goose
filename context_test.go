ef4257a337039182a738e6e03feb2367313d8848
zhoudaxia233 <yootaoo@gmail.com>
2020-05-20 13:55:46 +0200

    added context_test.go

---
+++ context_test.go
@@ -0,0 +1,26 @@
+package goose
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+)
+
+func TestContextQuery(t *testing.T) {
+	params := url.Values{}
+	params.Set("name", "Zheng Zhou")
+	params.Set("email", "go.ose@goose.com")
+	req := httptest.NewRequest(http.MethodGet, "/?"+params.Encode(), nil)
+	ctx := newContext(nil, req)
+
+	want := "Zheng Zhou"
+	if got := ctx.Query("name"); got != want {
+		t.Errorf("Want: %s , Got: %s", want, got)
+	}
+
+	want = "go.ose@goose.com"
+	if got := ctx.Query("email"); got != want {
+		t.Errorf("Want: %s , Got: %s", want, got)
+	}
+}
